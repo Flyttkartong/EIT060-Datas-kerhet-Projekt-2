c67696e3b6d538997add85dbc005f758ae3a2937
Samuel Johansson <johansson.samuel@live.se>
2015-02-24 22:12:53 +0100

    Removed treating list in Doctor (not used)

--- Server/src/user/Doctor.java
+++ Server/src/user/Doctor.java
@@ -3,21 +3,7 @@ package user;
 import java.util.ArrayList;
 
 public class Doctor extends Staff {
-	private ArrayList<String> treating;
-
 	public Doctor(String userID, String division) {
 		super(userID, division);
-	}
-	
-	public void addPatient(String patientID) {
-		treating.add(patientID);
-	}
-	
-	public void removePatient(String patientID) {
-		treating.remove(patientID);
-	}
-	
-	public ArrayList<String> getTreatingList(){
-		return treating;
 	}
 }
