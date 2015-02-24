6483b9fb481576c30e95a370219ae197b577f609
Samuel Johansson <johansson.samuel@live.se>
2015-02-24 10:20:13 +0100

    Updated AccessMonitor

--- Server/src/secure/AccessMonitor.java
+++ Server/src/secure/AccessMonitor.java
@@ -1,9 +1,23 @@
 package secure;
 
+import user.GA;
+import user.User;
+import data.ID;
+import data.MedicalRecord;
+
 public class AccessMonitor {
 
 	public AccessMonitor() {
-
+		
 	}
 
+	public boolean authenticate(ID ID){
+		return true;
+	}
+	
+	public void removeMedicalRecord(User user, MedicalRecord mr){
+		if(user instanceof GA){
+			mr.delete();
+		}
+	}
 }
