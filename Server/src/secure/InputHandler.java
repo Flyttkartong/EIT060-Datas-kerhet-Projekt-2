de23be6fe60b57da4561f87f77aec630dd025f33
Samuel Johansson <johansson.samuel@live.se>
2015-02-24 21:58:46 +0100

    Added class InputHandler

--- Server/src/secure/InputHandler.java
+++ Server/src/secure/InputHandler.java
@@ -1,8 +1,88 @@
 package secure;
 
 public class InputHandler {
+	private AccessController accessController;
+	
+	public InputHandler(AccessController ac) {
+		accessController = ac;
+	}
 
-	public void handleCommand(String command) {
+	public String handleCommand(String c) throws ArrayIndexOutOfBoundsException {
+		/*Split around quotation marks to get data - always last element (will have length 1 if no data exists) */
+		String[] dataInput = c.split("\"");
 		
+		/* If data exists, set flag */
+		boolean dataExists = false;
+		if(dataInput.length > 1) {
+			dataExists = true;
+		}
+
+		/* Split input around spaces and add dataInput if it exists */
+		String[] input = dataInput[0].split(" ");
+
+		/* Separate command from arguments */
+		String command = input[0];
+
+		String[] arguments;
+		/* If data exists, make room for it in arguments */
+		if (dataExists) {
+			arguments = new String[input.length];
+		} else {
+			arguments = new String[input.length - 1];
+		}
+		
+		for (int i = 0; i < input.length; i++) {
+			arguments[i] = input[i+1];
+		}
+		
+		/* If data exists, add it to arguments */
+		if(dataExists){
+			arguments[arguments.length - 1] = dataInput[1];
+		}
+
+		String returnValue;
+		/* Handle each command in a separate method */
+		switch (command) {
+		case "read":
+			returnValue = handleRead(arguments);
+			break;
+		case "write":
+			returnValue = handleWrite(arguments);
+			break;
+		case "remove":
+			returnValue = handleRemove(arguments);
+			break;
+		case "create":
+			returnValue = handleCreate(arguments);
+			break;
+		default:
+			returnValue = "Command: " + command + " does not exist.";
+			break;
+		}
+		return returnValue;
+	}
+
+	private String handleRead(String[] arguments) throws ArrayIndexOutOfBoundsException {
+		String mrID = arguments[0];
+		return accessController.read(mrID);
+	}
+
+	private String handleWrite(String[] arguments) throws ArrayIndexOutOfBoundsException {
+		String mrID = arguments[0];
+		String data = arguments[1];
+		return accessController.write(mrID, data);
+	}
+
+	private String handleRemove(String[] arguments) throws ArrayIndexOutOfBoundsException {
+		String mrID = arguments[0];
+		return accessController.remove(mrID);
+	}
+
+	private String handleCreate(String[] arguments) throws ArrayIndexOutOfBoundsException {
+		String mrID = arguments[0];
+		String patientID = arguments[1];
+		String nurseID = arguments[2];
+		String data = arguments[3];
+		return accessController.create(mrID, patientID, nurseID, data);
 	}
 }
